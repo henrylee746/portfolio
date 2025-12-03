3566bef55a596bdc515d7e8ec04ec1a4dce1566e
henrylee746 <henry.lee746@gmail.com>
2025-12-03 18:29:01 -0500

    Update dependencies, also patches CVE-2025-55182 security vulnerability

--- website/src/components/ui/bento-grid.tsx
+++ website/src/components/ui/bento-grid.tsx
@@ -1,8 +1,6 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
-import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 
 interface BentoGridProps extends ComponentPropsWithoutRef<"div"> {
   children: ReactNode;
